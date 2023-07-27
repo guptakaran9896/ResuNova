c0e3d0e8550f688e4524df15b75af8c4b7b933b9
karan <karan999867@gmail.com>
2023-07-27 17:52:05 +0530

    ResuNova

---
+++ src/components/Layouts/Footer.js
@@ -0,0 +1,45 @@
+import {
+    Text,
+    Stack,
+    HStack,
+    Box,
+    Container,
+    IconButton,
+    useColorModeValue
+} from '@chakra-ui/react';
+import { FaInstagram, FaLinkedin, FaGithub } from 'react-icons/fa';
+
+const Footer = () => {
+    return (
+        <Box
+            bg={useColorModeValue('gray.100', 'gray.900')}
+            color={useColorModeValue('gray.700', 'gray.200')}>
+
+            <Container
+                as={Stack}
+                maxW={'7xl'}
+                py={5}
+                direction={{ base: 'column', md: 'row' }}
+                spacing={4}
+                justify={{ base: 'center', md: 'space-between' }}
+                align={{ base: 'center', md: 'center' }}>
+
+                <Stack direction={'row'} spacing={6}>
+                    <IconButton isRound='true' as={'a'} href={'https://github.com/guptakaran9896'} target='_blank' bg={'gray.300'}>
+                        <FaGithub />
+                    </IconButton>
+                    <IconButton isRound='true' as={'a'} href={'https://www.linkedin.com/in/karan-gupta-97571b202/'} target='_blank' colorScheme={'blue'}>
+                        <FaLinkedin />
+                    </IconButton>
+                    <IconButton isRound='true' as={'a'} href={'https://www.instagram.com/jigarsable_/'} target='_blank' colorScheme={'pink'}>
+                        <FaInstagram />
+                    </IconButton>
+
+                </Stack>
+
+            </Container>
+        </Box>
+    )
+}
+
+export default Footer
